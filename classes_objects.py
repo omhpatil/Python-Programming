e0e25d57c4ab01ff67afff8090e3102f720c2e04
Om Patil <97589129+omhpatil@users.noreply.github.com>
2023-03-28 10:34:25 +0530

    Add files via upload

---
+++ classes_objects.py
@@ -0,0 +1,11 @@
+# python is an object oriented language 
+# Almost everything in Python is an object, with its properties and methods.
+# A Class is like an object constructor, or a "blueprint" for creating objects.
+
+class myclass: # class 
+    x = 5 # proporty named x
+c = myclass() # object created
+print(c.x)
+
+
+        
